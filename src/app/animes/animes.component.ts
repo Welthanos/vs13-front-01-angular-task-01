1c28800d0f2e954e0177747ae08d81a2765f8717
Welthanos <welthanos_dev@outlook.com>
2024-02-20 10:38:45 -0300

    feat:top 5 animes page

---
+++ src/app/animes/animes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animes',
+  standalone: true,
+  imports: [],
+  templateUrl: './animes.component.html',
+  styleUrl: './animes.component.scss'
+})
+export class AnimesComponent {
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LolMasteryComponent } from './lol-mastery/lol-mastery.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CounterPageComponent } from './counter-page/counter-page.component';
+import { AnimesComponent } from './animes/animes.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/lol-mastery', pathMatch: 'full' },
     { path: 'lol-mastery', component: LolMasteryComponent },
     { path: 'counter', component: CounterPageComponent },
+    { path: 'animes', component: AnimesComponent },
     { path: '**', component: PageNotFoundComponent },
 ];
