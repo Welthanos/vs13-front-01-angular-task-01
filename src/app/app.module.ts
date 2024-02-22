7ad97e5be3dafade8571dc9d1465c3e94a7573c9
Welthanos <welthanos_dev@outlook.com>
2024-02-22 09:57:06 -0300

    feat: partial implement of json request

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    declarations: [
+    ],
+    imports: [
+        HttpClientModule,
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: []
+})
+export class AppModule { }
