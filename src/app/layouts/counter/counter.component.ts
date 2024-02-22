c22e64c09f118c91924ea3094e24918d3f06448e
Welthanos <welthanos_dev@outlook.com>
2024-02-22 12:13:37 -0300

    refactor: kda counter

--- src/app/layouts/counter/counter.component.ts
+++ src/app/layouts/counter/counter.component.ts
@@ -8,13 +8,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
+  @Input() kda!: number;
   @Input() kills!: number;
   @Input() deaths!: number;
   @Input() assistences!: number;
+  @Output() kdaChange = new EventEmitter<number>();
   @Output() killsChange = new EventEmitter<number>();
   @Output() deathsChange = new EventEmitter<number>();
   @Output() assistencesChange = new EventEmitter<number>();
 
+  kdaResult() { this.calculateKDA(this.kills, this.deaths, this.assistences) }
   incrementKill() { this.kill(+1) }
   incrementDeath() { this.die(+1) }
   incrementAssistence() { this.assist(+1) }
@@ -22,13 +25,21 @@ export class CounterComponent {
   kill(delta: number) {
     this.kills = this.kills + delta;
     this.killsChange.emit(this.kills);
+    this.calculateKDA(this.kills, this.deaths, this.assistences);
   }
   die(delta: number) {
     this.deaths = this.deaths + delta;
     this.deathsChange.emit(this.deaths);
+    this.calculateKDA(this.kills, this.deaths, this.assistences);
   }
   assist(delta: number) {
     this.assistences = this.assistences + delta;
     this.assistencesChange.emit(this.assistences);
+    this.calculateKDA(this.kills, this.deaths, this.assistences);
+  }
+  calculateKDA(kills: number, deaths: number, assistences: number) {
+    if (deaths == 0) this.kda = kills + assistences;
+    else this.kda = ((kills + assistences) / deaths);
+    this.kdaChange.emit(this.kda);
   }
 }
--- src/app/pages/counter-page/counter-page.component.ts
+++ src/app/pages/counter-page/counter-page.component.ts
@@ -9,6 +9,7 @@ import { CounterComponent } from '../../layouts/counter/counter.component';
   styleUrl: './counter-page.component.scss'
 })
 export class CounterPageComponent {
+  kda = 0;
   kills = 0;
   deaths = 0;
   assistences = 0;
