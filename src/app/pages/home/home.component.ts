0d797d91420361967253151fc36091461c3fce6b
Welthanos <welthanos_dev@outlook.com>
2024-02-22 10:44:27 -0300

    feat: json request on home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private http: HttpClient) { }
+
   title = 'Bem-vindos à página do jogador Welthanos!';
-  profile = {
-    images: [
-      '../../assets/profile-1.png',
-      '../../assets/profile-2.png',
-      '../../assets/elo.png'
-    ]
+  profile = { images: [] };
+
+  ngOnInit() {
+    this.http.get<any>('../../../assets/data.json').subscribe((data) => {
+      this.profile = data.profile;
+      console.log(this.profile.images);
+    });
   }
 }
